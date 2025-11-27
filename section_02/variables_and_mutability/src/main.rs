e91a15c8dc791332d6ab96730e7cf15ee62dcedd
JFicchini <jficchini.dev@gmail.com>
2025-11-27 14:25:19 -0300

    Section 2 update

--- section_02/variables_and_mutability/src/main.rs
+++ section_02/variables_and_mutability/src/main.rs
@@ -4,8 +4,22 @@ fn main() {
     let _fruits = apples + oranges;
 
     println!(
-        "This year, my garden has {0} apples and {1} oranges.\
-         I can't believe I have {0} apples.",
+        "\nThis year, my garden has {0} apples and {1} oranges.\nI can't believe I have {0} apples.\n",
         apples, oranges
     );
+
+    // Mutable and imutable
+    // Variables are imutable as default. If you want to change a variable turn to mutable with "mut"
+
+    let mut gym_reps = 10;
+    println!("I plan to do {gym_reps} reps");
+
+    gym_reps = 15;
+    println!("I now plan to do {gym_reps} reps\n");
+
+    /*
+    Check documentation to see all Errors at Rust.
+    To see the error and solution at terminal run 'rustc --explain E0384(ERROR_NUMBER)'
+    */
+
 }
